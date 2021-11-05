32868273df48e2753dc3a2b03b10a81dbfcc0ba4
Marian Soza <43736122+marianSoza99@users.noreply.github.com>
2021-11-04 21:33:17 -0600

    Create server.py

---
+++ server.py
@@ -0,0 +1,55 @@
+import socket as s
+import variables as v
+import threading
+
+sockets = []
+
+socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+socket.setsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
+socket.bind((v.HOST, v.PORT))
+socket.listen(v.CLIENTS)
+
+sockets.append(socket)
+print(f'Server on {v.HOST}:{v.PORT}')
+
+# recibe el mensaje de un cliente
+def message(socket): 
+    try:
+        msg = socket.recv(v.BUFFER_SIZE)
+        if not len(msg):
+            return False     
+        return msg
+    except:
+        return False
+
+# envia el mensaje a todos los clientes que no son el emisor
+def broadcast(message, sock):
+    for client in sockets:
+        if client != sock:
+            client.send(message)
+
+# hilo del cliente
+def socket_thread(conn, address):
+    while True:
+        msg = message(conn)
+        if msg:
+            print(type(msg))
+            broadcast(msg, conn)
+        else:
+            print("(%s, %s) is offline\n" % address)
+            sockets.remove(conn)
+            break
+    conn.close()
+
+# crea el hilo del cliente
+def new_thread(conn, address):
+    thread = threading.Thread(target = socket_thread, args=(conn, address, ))
+    thread.start()
+    return 
+
+while True:
+    conn, address = socket.accept()
+    sockets.append(conn)
+    print("(%s, %s) connected" % address)
+    new_thread(conn, address)
+    
